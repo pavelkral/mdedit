96430542fa44564f6953d2f9a3d9f35cd7323953
Pavel Kral <frostxx@tiscali.cz>
2025-06-10 09:08:59 +0200

    Enhances Markdown to HTML conversion
    Improves the Markdown to HTML conversion process by adding code block and inline code formatting, heading, emphasis, link, and list support.
    
    Adds Fusion style to the application.
    
    Removes unused action from the UI and cleans up UI styles.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 
 #include <QApplication>
-
+#include <QStyleFactory>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setStyle(QStyleFactory::create("Fusion"));
     MainWindow window;
     window.show();
     return app.exec();
